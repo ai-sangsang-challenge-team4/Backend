8618d3156bceebf7c4a2c9d84c35d8cef1f046d1
seoah <24seoah@sookmyung.ac.kr>
2026-08-05 22:33:20 +0900

    chore : 초기 세팅

---
+++ src/main/java/com/teacherhub/TeacherhubApplication.java
@@ -0,0 +1,13 @@
+package com.teacherhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherhubApplication.class, args);
+    }
+
+}
